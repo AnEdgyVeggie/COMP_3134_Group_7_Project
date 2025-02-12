31058cf6e3ea7a51262723c0ff8a4d767702cadb
ethan <ethan.sylvester@gmx.com>
2025-02-12 09:41:02 -0500

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Ethan Sylvester on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
