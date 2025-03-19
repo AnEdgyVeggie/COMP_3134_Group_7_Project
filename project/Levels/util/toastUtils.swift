123e095800a5610ae5d35768e14b71cec06a3aeb
Catherine Martin <catherinemartin@Catherines-MacBook-Air.local>
2025-03-19 15:37:45 -0400

    start of utils file for toast message

---
+++ project/Levels/util/toastUtils.swift
@@ -0,0 +1,9 @@
+// Toast Utility Function
+// This file holds the design / functionality for a toast
+// To be displayed to the player when they've made an incorrect guess
+
+import SwiftUI
+
+struct ToastView: View {
+    let toastMessage: String = "Incorrect. Try again!"
+}
