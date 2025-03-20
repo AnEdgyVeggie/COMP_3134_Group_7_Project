efbb139fa56580f27c7f9000e1185105af273554
Taylor Martin <taylormartind@gmail.com>
2025-03-20 12:36:17 -0400

    function for toast to disappear after 2 seconds

--- project/Levels/util/ToastUtils.swift
+++ project/Levels/util/ToastUtils.swift
@@ -1,31 +1,23 @@
 // Toast Utility Function
-// This file holds the design / functionality for a toast
+// This file holds the functionality for a toast
 // To be displayed to the player when they've made an incorrect guess
+// (disappears on 3 seconds)
 
+import Combine
 import SwiftUI
 
-struct ToastView: View {
-    // message displayed in toast
-    let toastMessage: String = "Incorrect. Try again!"
+func toggleToast (isVisible: Binding<Bool>, toastTimer: inout Timer?,
+                  duration: TimeInterval = 2) {
+    // when function is called isVisible set to true (shows toast)
+    isVisible.wrappedValue = true
     
-    // boolean to determine whether toast is visible
-    @Binding
-    var isVisible: Bool
+    // stops multiple toasts popping up
+    toastTimer?.invalidate()
     
-    var body: some View {
-        // toast design
-        if isVisible {
-            ZStack {
-                Color(red: 0.92, green: 0.85, blue: 0.7)
-                    // removing white space below toast
-                    .ignoresSafeArea(.all, edges: .bottom)
-                
-                Text(toastMessage)
-                    .font(.system(size: 24, weight: .bold))
-                    .foregroundStyle(.black)
-                    .padding()
-            }
-            .frame(maxHeight: 35)
+    toastTimer = Timer.scheduledTimer(withTimeInterval: duration, repeats: false) {_ in
+        DispatchQueue.main.async {
+            // hide toast after timer registers 3 seconds
+            isVisible.wrappedValue = false
         }
     }
 }
