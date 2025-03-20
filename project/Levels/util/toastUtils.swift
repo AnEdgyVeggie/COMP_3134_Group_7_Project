3c533916645432d28450596021dd78404e4d39de
Taylor Martin <taylormartind@gmail.com>
2025-03-20 09:44:16 -0400

    toast message functioning, needs styling + conditions

--- project/Levels/util/toastUtils.swift
+++ project/Levels/util/toastUtils.swift
@@ -5,5 +5,24 @@
 import SwiftUI
 
 struct ToastView: View {
+    // message displayed in toast
     let toastMessage: String = "Incorrect. Try again!"
+    
+    // boolean to determine whether toast is visible
+    @Binding
+    var isVisible: Bool
+    
+    var body: some View {
+        // toast design
+        VStack {
+            if isVisible {
+                Text(toastMessage)
+                    .font(.system(size: 24, weight: .bold))
+                    .background(Color.red)
+                    .padding(5)
+                    .cornerRadius(10)
+            }
+        }
+        .foregroundStyle(.teal)
+    }
 }
